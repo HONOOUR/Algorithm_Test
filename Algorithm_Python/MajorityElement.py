5787a90e854d0a8ee24803f95c6eeb4aa43daab3
Honoour <hearful@icloud.com>
2021-04-29 00:11:16 +0900

    divide-and-conquer

--- Algorithm_Python/MajorityElement.py
+++ Algorithm_Python/MajorityElement.py
@@ -8,12 +8,25 @@ class MajorityElement:
             else:
                 majority[num] = 1
 
-        for value in majority.values():  # or index for i in range(len(majority))
-            if value > count:           # majority[i]
-                count = value
+            if majority[num] > len(nums) // 2:
+                return num
 
-        return count
+        return None
+
+    def getMaroityElement_divide_conquer(self, nums: list) -> int:
+        if not nums:
+            return None
+        if len(nums) == 1:
+            return nums[0]
+
+        half = len(nums) // 2
+        front_half = self.getMaroityElement_divide_conquer(nums[:half])
+        back_half = self.getMaroityElement_divide_conquer(nums[half:])
+
+        return [back_half, front_half][nums.count(
+            front_half) > len(nums) // 2]
 
 
 instance = MajorityElement()
 print(instance.getMaroityElement([2, 2, 1, 1, 1, 2, 2]))
+print(instance.getMaroityElement_divide_conquer([2, 2, 1, 1, 1, 2, 2]))
