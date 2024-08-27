0a8d6cdb4fb46a314c90b93b9122df02381b9f86
Honoour <hearful@icloud.com>
2024-08-27 22:16:20 +0900

    python solutions

--- Algorithm_Python/해커랭크/Arrays.py
+++ Algorithm_Python/해커랭크/Arrays.py
@@ -1,4 +1,5 @@
-# https://www.hackerrank.com/challenges/2d-array/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
+# https://www.hackerrank.com/
+# challenges/2d-array/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
 # 1 제일 큰 합
 # arr[i][j] + arr[i][j+1] + arr[i][j+2] 
 # + arr[i+1][j+1] 
@@ -26,19 +27,9 @@ hourglassSum(arr)
 
 
 def rotLeft(a, d):
-    # a = [1, 2, 3, 4, 5]
-    # length = len(a) = 5
-    # d = 2
-    # a[|0-2+5|%5] = a[3] = 1
-    # a[|1-2+5|%5] = a[4] = 2
-    # a[|2-2|%5] = a[0] = 3
-    # a[|5-2|%5] = a[3] = 5
-    
     n = len(a)
     answer = [0 for _ in range(n)]
     for i in range(n):
-        if i-d<0:
-            answer[abs(i+n-d) % n] = a[i]
-        else:
-            answer[abs(i-d) % n] = a[i]
+        answer[abs(i+n-d) % n] = a[i]
+        
     return answer
--- Algorithm_Python/해커랭크2/Arrays.py
+++ Algorithm_Python/해커랭크2/Arrays.py
@@ -1,21 +1,15 @@
 # https://www.hackerrank.com/challenges/new-year-chaos/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
 def minimumBribes(q):
-    # more than 2 
     for i in range(len(q)-1, -1, -1):
         if i+1 < q[i]-2:
             print('Too chaotic')
             return
-    # bribe count
     bribe_count = 0
-    for i in range(len(q)):
-        for j in range(max(0, q[i]-2), i):
+    for i in range(len(q)): # to i 
+        for j in range(max(0, q[i]-2), i): # count bigger number before i
             if q[i] < q[j]:
-                temp = q[i]
-                q[i] = q[j]
-                q[j] = temp
                 bribe_count += 1
     print(bribe_count)
-
 
 
 def minimumSwaps(arr):
