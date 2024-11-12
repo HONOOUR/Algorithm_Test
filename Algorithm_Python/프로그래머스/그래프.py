b59aeb2111afb2220d4b4a0b1c86e00c8179d0ff
jieun.choi <jieun.choi@papamobility.com>
2024-11-12 16:50:33 +0900

    graph

---
+++ Algorithm_Python/프로그래머스/그래프.py
@@ -0,0 +1,20 @@
+from collections import deque, Counter
+
+def solution(begin, target, words):
+    visited = set()
+    queue = deque([(begin, 0)])
+    while queue:
+        # 1. 한개의 단어만 다른지 체크
+        word_begin, count = queue.popleft()
+        if word_begin == target:
+            return count
+        if count > len(words):
+            break
+        visited.add(word_begin)
+        for word in words:
+            if word not in visited:
+                if sum([1 for a, b in zip(word_begin, word) if a != b]) == 1:
+                    # 2. 큐에 넣기 (word, count+1)
+                    queue.append((word, count+1))
+
+    return 0
--- Algorithm_Python/프로그래머스/우선탐색.py
+++ Algorithm_Python/프로그래머스/우선탐색.py
@@ -22,4 +22,44 @@ def solution(n, computers):
         
     return answer
 
-solution(3, [[1, 1, 0], [1, 1, 0], [0, 0, 1]])
+# solution(3, [[1, 1, 0], [1, 1, 0], [0, 0, 1]])
+
+def solution(n, computers):
+    def dfs(node, visited):
+        visited.add(node)
+        for i in range(n):
+            if computers[node][i] == 1 and i not in visited:
+                dfs(i, visited)
+
+    visited = set()
+    answer = 0
+
+    for i in range(n):
+        if i not in visited:
+            dfs(i, visited)
+            answer += 1
+    
+    return answer
+
+def solution(maps):
+    is_solved = False
+    min_count = 25
+    def dfs(x, y, count, visited):
+        visited.add((x,y))
+        if x == len(maps[0])-1 and y == len(maps)-1:
+            global is_solved
+            global min_count
+            is_solved = True
+            min_count = min(min_count, count)
+        
+        for xd, yd in [[0,1], [0,-1], [-1,0], [1,0]]:
+            if 0 <= x+xd < len(maps[0]) and 0 <= y+yd < len(maps) and (x+xd, y+yd) not in visited:
+                if maps[y+yd][x+xd] == 1:
+                    dfs(x+xd, y+yd, count+1, visited)
+                    visited.remove((x+xd, y+yd))
+    visited = set()
+    dfs(0, 0, 0, visited)
+    if is_solved:
+        return min_count
+    return -1
+solution([[1, 0, 1, 1, 1], [1, 0, 1, 0, 1], [1, 0, 1, 1, 1], [1, 1, 1, 0, 1], [0, 0, 0, 0, 1]])
